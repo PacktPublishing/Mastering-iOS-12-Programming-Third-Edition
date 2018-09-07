f0646f8dadb69eea0d242bb3ade6b0ef0b930a20
Donny Wals <donny.wals@bamtechmedia.com>
2018-09-07 14:44:36 +0200

    removed some header comments

--- chapter 20/ARGallery_start/ARGallery/Cells/GalleryCollectionItem.swift
+++ chapter 20/ARGallery_start/ARGallery/Cells/GalleryCollectionItem.swift
@@ -1,11 +1,3 @@
-//
-//  GalleryCollectionItem.swift
-//  AugmentedRealityGallery
-//
-//  Created by Donny Wals on 29/07/2017.
-//  Copyright © 2017 DonnyWals. All rights reserved.
-//
-
 import UIKit
 
 class GalleryCollectionItem: UICollectionViewCell {
